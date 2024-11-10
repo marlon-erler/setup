402a8bdc3ff8ae608c1947f21fc98b5cb468afc7
marlon-erler <viridian035@gmail.com>
2024-11-10 17:53:47 +0000

    add build script

---
+++ build.sh
@@ -0,0 +1,1 @@
+podman build -t debian1 ./A/podman/debian
--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
- podman run -v ./Documents/:/root/Documents -v ./A:/root/A -ti debian1
+ podman run -v ./Documents/:/home/vd/Documents -v ./A:/home/vd/A -p 5901:5901 -ti debian1
