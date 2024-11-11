bb3bda63849dd0ce460d7b464e5418c70137baf2
marlon-erler <viridian035@gmail.com>
2024-11-11 12:31:24 +0100

    allow setting vnc port

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-podman run -v ./ssh:/home/vd/.ssh -v ./Documents/:/home/vd/Documents -v ./A:/home/vd/A -p 5901:5901 -ti debian1
+if [ -z "$1" ]; then
+	echo "number not specified"
+	exit
+fi
+
+podman run -v ./ssh:/home/vd/.ssh -v ./Documents/:/home/vd/Documents -v ./A:/home/vd/A -p 590$1:5901 -ti debian1
