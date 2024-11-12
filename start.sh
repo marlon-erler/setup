4b1c15e0d74e5d12af1467ecc5b0aebe03ad78bc
marlon-erler <viridian035@gmail.com>
2024-11-12 12:24:20 +0000

    support 10 ports for localhost servers

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ if [ -z "$1" ]; then
 	exit
 fi
 
-podman run -v ./vim:/home/vd/.vim -v ./vnc:/home/vd/.vnc -v ./ssh:/home/vd/.ssh -v ./mozilla:/home/vd/.mozilla -v ./Documents/:/home/vd/Documents -v ./A:/home/vd/A -p 590$1:5901 -p 808$1:8080 -ti debian1
+podman run -v ./vim:/home/vd/.vim -v ./vnc:/home/vd/.vnc -v ./ssh:/home/vd/.ssh -v ./mozilla:/home/vd/.mozilla -v ./Documents/:/home/vd/Documents -v ./A:/home/vd/A -p 590$1:5901 -p 80$10-80$19:8000-8009 -ti debian1
